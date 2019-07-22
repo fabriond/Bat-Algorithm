173ac6192ace7b48504cb7cfc2249a49704ab2f4
fabriond <gfbf@ic.ufal.br>
2019-07-22 04:48:38 -0300

    Fixed constants

--- cpp_code/bat/bat.h
+++ cpp_code/bat/bat.h
@@ -5,10 +5,10 @@
 #include <vector>
 
 #define MIN_FREQ 0.0
-#define MAX_FREQ 100.0
+#define MAX_FREQ 1.0
 
 #define MIN_LOUDNESS 0.0
-#define MAX_LOUDNESS 10.0
+#define MAX_LOUDNESS 1.5
 
 #define MIN_PULSE 0.0
 #define MAX_PULSE 1.0
